eecbe6fc27aee44a7f4002b2e5549ff02a5cded0
DimaDenis <vs94dd@gmail.com>
2015-11-25 14:40:51 +0200

    as

---
+++ site-needs/js/functions.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Denis on 25.11.2015.
+ */
+$(document).ready(function() {
+    $count=0;
+    $(".hidden-p").hide();
+    $(".show-sep").hide();
+
+
+    $(".visible-p").click(function(){
+        if($count==0){
+            $(".hidden-p").show(300);
+            $(".show-sep").show();
+            $(".hidden-sep").hide();
+            $count=1;
+        }
+        else{
+            $(".hidden-p").hide(300);
+            $(".show-sep").hide();
+            $(".hidden-sep").show();
+            $count=0;
+        }
+
+    });
+
+});
+
+
